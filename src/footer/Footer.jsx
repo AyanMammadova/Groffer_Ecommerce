fb76cf6b016e54d5a5187a2106f02fb569d73aca
AyanMammadova <ayanmamedova128@gmail.com>
2025-02-01 22:34:37 +0400

    nese

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { IoLocationSharp } from 'react-icons/io5'
+import { MdOutlineEmail } from 'react-icons/md'
 
 function Footer() {
   return (
     <>
-      <div className='h-[200px] bg-green-800'>
-
-      </div>
+      <footer className="bg-[url('https://groffer.modeltheme.com/wp-content/uploads/2023/01/Footer-BG.png')] bg-cover bg-start w-[100%] ">
+        <div className='sm:flex font-[500] mb-[30px] px-[10px] md:mx-[30px] py-[30px] border-b-[1px] border-gray-200 justify-between flex-wrap'>
+          <ul className='sm:flex text-[1.3em] gap-[20px]'>
+            <li>My Account</li>
+            <li>Bag</li>
+            <li>Personal info</li>
+            <li>Wishlist</li>
+          </ul>
+          <ul className='flex gap-[10px]'>
+            <li><FaFacebook /></li>
+            <li><FaTwitter /></li>
+            <li><FaInstagram /></li>
+          </ul>
+        </div>
+        <div className='px-[10px] md:mx-[30px] border-b-[1px] border-gray-200 lg:flex flex-wrap py-[30px] justify-around text-[1.2em]'>
+          <div className='w-[95%] mx-auto lg:w-[25%] my-[20px]'>
+            <img className='w-[100px] py-[10px]' src="https://groffer.modeltheme.com/wp-content/uploads/2023/01/Groffer-Logo.svg" alt="" />
+            <p className='text-gray-600 py-[20px]'>Online grocery shopping and delivery
+              at Groffer. Buy fruits, bread, vegetables.</p>
+            <div>
+              <img src="https://groffer.modeltheme.com/wp-content/uploads/2023/02/footer-apps-300x43.png" alt="" />
+            </div>
+          </div>
+          <div className='w-[95%] mx-auto lg:w-[15%] my-[20px]'>
+            <p className='font-[500] text-[1.2em] pb-[20px]'>Quick Links</p>
+            <ul className='text-gray-600'>
+              <li>Our Services</li>
+              <li>About Company</li>
+              <li>Latest News</li>
+              <li>Meet The Team</li>
+              <li>Our Products</li>
+            </ul>
+          </div>
+          <div className='w-[95%] mx-auto lg:w-[15%] my-[20px]'>
+            <p className='font-[500] text-[1.2em] pb-[20px]'>Account</p>
+            <ul className='text-gray-600'>
+              <li>Your Account</li>
+              <li>Shipping rates</li>
+              <li>Refunds</li>
+              <li>Delivery info</li>
+            </ul>
+          </div>
+          <div className='w-[95%] mx-auto lg:w-[15%] my-[20px]'>
+            <p className='font-[500] text-[1.2em] pb-[20px]'>About Us</p>
+            <ul className='text-gray-600'>
+              <li>About Company</li>
+              <li>Our team</li>
+              <li>Contacts</li>
+              <li>News&Event</li>
+              <li>Prder Tracking</li>
+            </ul>
+          </div>
+          <div className='w-[95%] mx-auto lg:w-[25%] my-[20px]'>
+            <p className='text-[500] text-[1.3em] font-[500] pb-[20px]'>Don’t hesitate to contact us or call !</p>
+            <p className='flex gap-[10px] text-gray-600 py-[10px]'>
+              <IoLocationSharp className='text-[#136450] text-[1.5em] '/> 4 Grand Central Terminal New York, NY 10 City name, CA 90896 United State USA.
+            </p>
+            <p className='flex  text-gray-600 gap-[10px]'>
+              <MdOutlineEmail className='text-[#136450] text-[1.2em]'/> contact@example.com
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className='text-[1.2em] text-center py-[20px]'>Copyright by . All Rights Reserved.</p>
+        </div>
+      </footer>
     </>
   )
 }
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { FaRegStar } from 'react-icons/fa6'
+import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
 
 function Main() {
   return (
@@ -345,14 +351,14 @@ function Main() {
 
 
       {/* 2 DENE BALACA REKLAM */}
-      <div className='hidden sm:grid grid-cols-1 gap-[30px] w-[95%] mx-auto sm:grid-cols-2'>
-        <div className='relative  w-[100%]'>
-          <img className='inset-0' src="https://groffer.modeltheme.com/wp-content/uploads/2023/02/Main-banners01.png" alt="" />
+      <div className='hidden sm:grid grid-cols-1 gap-[30px]  w-[95%] mx-auto sm:grid-cols-2'>
+        <div data-aos="fade-right" className=' group relative overflow-hidden w-[100%]'>
+          <img className='inset-0 group-hover:scale-105 transition-all duration-200' src="https://groffer.modeltheme.com/wp-content/uploads/2023/02/Main-banners01.png" alt="" />
           <p className='text-gray-400 left-[20px] absolute top-[40px]'>10%  At Any Nearest Stores</p>
           <p className='font-[500] w-[50%] left-[20px] text-[1.5em] md:text-[3em] top-[60px] absolute'>30% off on All veggies</p>
         </div>
-        <div className='relative  w-[100%]'>
-          <img className='inset-0' src="https://groffer.modeltheme.com/wp-content/uploads/2023/02/Main-banner02.jpeg" alt="" />
+        <div data-aos="fade-left" className='group relative overflow-hidden w-[100%]'>
+          <img className='inset-0 group-hover:scale-105 transition-all duration-200' src="https://groffer.modeltheme.com/wp-content/uploads/2023/02/Main-banner02.jpeg" alt="" />
           <p className='text-gray-400 left-[20px] absolute top-[40px]'>10%  At Any Nearest Stores</p>
           <p className='font-[500] w-[50%] left-[20px] text-[1.5em] md:text-[3em] top-[60px] absolute'>30% off on All veggies</p>
         </div>
@@ -381,6 +387,89 @@ function Main() {
           <img className='hover:scale-105 transition-all duration-100' src="https://groffer.modeltheme.com/wp-content/uploads/2023/01/Main_service4-150x150.png" alt="" />
           <p className='font-bold pt-[10px]'>Delicious & New Food</p>
           <p className='text-gray-500 pt-[10px]'>Feel great about what you eat every day</p>
+        </div>
+      </div>
+
+      {/* CLIENT REVIEWS */}
+      <div className='relative mt-[40px]  w-[100%]'>
+        <img className='inset-0 w-[100%] h-[80vh]' src="https://groffer.modeltheme.com/wp-content/uploads/2023/01/Main-testimonials_bg.png?id=8701" alt="" />
+        <img className='md:absolute md:-top-[40px] md:block hidden md:right-[50px]' src="https://groffer.modeltheme.com/wp-content/uploads/2023/01/BG-02.png" alt="" />
+
+        <div className='absolute w-[100%] top-[100px]'>
+          <p className='text-center text-[#E1BE6E] text-[1.5em] font-[500]'>Clien Review</p>
+          <p className='text-center text-[2em] font-[500]'>From the People</p>
+          <div className='flex justify-between pt-[50px]'>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              pagination={{
+                clickable: true,
+              }}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false, 
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                },
+                1024: {
+                  slidesPerView: 3,
+                },
+              }}
+              
+              modules={[FreeMode, Pagination,Autoplay]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div className='flex cursor-grab px-[20px] flex-col  mx-auto  items-center justify-center text-center'>
+                  <img className='w-[80px] rounded-2xl' src="https://groffer.modeltheme.com/wp-content/uploads/2015/02/Main-testimon3-350x350.jpg" alt="" />
+                  <p className='text-gray-500 font-[400] pt-[10px]'>
+                    “What a beautiful haul from Groffer Market week! It is basically a chef’s dream. Definitely have a lot to work.”
+                  </p>
+                  <p className='text-[#448476] pb-[30px] font-[600] pt-[10px]'>Martin Culich</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='flex cursor-grab px-[20px] flex-col  mx-auto  items-center justify-center text-center'>
+                  <img className='w-[80px] rounded-2xl' src="https://groffer.modeltheme.com/wp-content/uploads/2015/02/Main-Testimon1-350x350.jpg" alt="" />
+                  <p className='text-gray-500 font-[400] pt-[10px]'>
+                    “What a beautiful haul from Groffer Market week! It is basically a chef’s dream. Definitely have a lot to work.”
+                  </p>
+                  <p className='text-[#448476] pb-[30px] font-[600] pt-[10px]'>Martin Culich</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='flex cursor-grab px-[20px] flex-col  mx-auto  items-center justify-center text-center'>
+                  <img className='w-[80px] rounded-2xl' src="https://groffer.modeltheme.com/wp-content/uploads/2015/02/Main-testimon5-350x350.jpg" alt="" />
+                  <p className='text-gray-500 font-[400] pt-[10px]'>
+                    “What a beautiful haul from Groffer Market week! It is basically a chef’s dream. Definitely have a lot to work.”
+                  </p>
+                  <p className='text-[#448476] pb-[30px] font-[600] pt-[10px]'>Martin Culich</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='flex cursor-grab px-[20px] flex-col  mx-auto  items-center justify-center text-center'>
+                  <img className='w-[80px] rounded-2xl' src="https://groffer.modeltheme.com/wp-content/uploads/2015/02/Main-Testimon2-350x350.jpg" alt="" />
+                  <p className='text-gray-500 font-[400] pt-[10px]'>
+                    “What a beautiful haul from Groffer Market week! It is basically a chef’s dream. Definitely have a lot to work.”
+                  </p>
+                  <p className='text-[#448476] pb-[30px] font-[600] pt-[10px]'>Martin Culich</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='flex cursor-grab px-[20px] flex-col  mx-auto  items-center justify-center text-center'>
+                  <img className='w-[80px] rounded-2xl' src="https://groffer.modeltheme.com/wp-content/uploads/2015/02/Main-testimon4-350x350.jpg" alt="" />
+                  <p className='text-gray-500 font-[400] pt-[10px]'>
+                    “What a beautiful haul from Groffer Market week! It is basically a chef’s dream. Definitely have a lot to work.”
+                  </p>
+                  <p className='text-[#448476] pb-[30px] font-[600] pt-[10px]'>Martin Culich</p>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+
+          </div>
         </div>
       </div>
     </>
