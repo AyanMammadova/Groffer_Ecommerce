fbafb148c5bff58f85d01c5cb238560dc4581441
AyanMammadova <ayanmamedova128@gmail.com>
2025-02-23 15:16:09 +0400

    success

--- src/components/main/Success.jsx
+++ src/components/main/Success.jsx
@@ -4,7 +4,7 @@ function Success() {
   return (
     <div className='h-[500px] text-center py-[100px]'>
      <p className='text-green-400'>THANK YOU FOR ORDER</p> 
-    </div>
+    </div> 
   )
 }
 
