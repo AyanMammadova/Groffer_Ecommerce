742340831194112dab196b4397ccbf755d586d38
AyanMammadova <ayanmamedova128@gmail.com>
2025-02-23 15:20:48 +0400

    css change

--- src/components/main/Success.jsx
+++ src/components/main/Success.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Success() {
   return (
     <div className='h-[500px] text-center py-[100px]'>
-     <p className='text-green-400'>THANK YOU FOR ORDER</p> 
+     <p className='text-green-400 text-[1.4em]'>THANK YOU FOR ORDER</p> 
     </div> 
   )
 }
